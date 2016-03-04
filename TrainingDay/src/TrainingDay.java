cce020b5510f4b0cec8d5dfe389c470b9ce53aa2
alex <alex@PasswordIsTaco>
2016-03-04 16:22:18 -0500

    huio

--- TrainingDay/src/TrainingDay.java
+++ TrainingDay/src/TrainingDay.java
@@ -5,6 +5,8 @@ public class TrainingDay {
 		// TODO Auto-generated method stub
 		System.out.println("titties for da boys");
 		System.out.println("titties for the girls too");
+		
+		System.out.println("getting tunrt");
 	}
 
 }
