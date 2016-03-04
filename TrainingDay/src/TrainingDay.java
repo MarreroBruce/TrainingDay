1e64fac27120bd20a87db01a903ebabe49f410ed
Alex Marrero <marreroalex2013@gmail.com>
2016-03-04 16:10:13 -0500

    for the girls too

--- TrainingDay/src/TrainingDay.java
+++ TrainingDay/src/TrainingDay.java
@@ -4,6 +4,7 @@ public class TrainingDay {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("titties for da boys");
+		System.out.println("titties for the girls too");
 	}
 
 }
